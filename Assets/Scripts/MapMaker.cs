5f292199b3982306116d5a4100fb0ea85f0ddc79
Mr-again <840155767@qq.com>
2020-02-03 23:36:11 -0800

    element click

---
+++ Assets/Scripts/MapMaker.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MapMaker : MonoBehaviour
+{
+    public Button wall;
+    public Button brown_box;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wall.onClick.AddListener(() => { OnClickElement(); });
+        brown_box.onClick.AddListener(() => { OnClickElement(); });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnClickElement()
+    {
+        var selected_element = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject;
+        Debug.Log("Click On Element: "+selected_element.name);
+
+    }
+}
