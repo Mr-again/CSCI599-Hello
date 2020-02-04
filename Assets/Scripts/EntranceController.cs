74f187b7d7eef6480c3474e7bec5a594ee43f38d
Mr-again <840155767@qq.com>
2020-02-03 22:27:04 -0800

    all scene added

--- Assets/Scripts/EntranceController.cs
+++ Assets/Scripts/EntranceController.cs
@@ -7,11 +7,15 @@ using UnityEngine.SceneManagement;
 public class EntranceController : MonoBehaviour
 {
     public Button stage_mode;
+    public Button community;
+    public Button leader_board;
 
     // Start is called before the first frame update
     void Start()
     {
         stage_mode.onClick.AddListener(() => { OnClickStageMode(); });
+        community.onClick.AddListener(() => { OnClickCommunity(); });
+        leader_board.onClick.AddListener(() => { OnClickLeaderBoard(); });
     }
 
     // Update is called once per frame
@@ -22,7 +26,17 @@ public class EntranceController : MonoBehaviour
 
     void OnClickStageMode()
     {
-        Debug.Log(111);
-        SceneManager.LoadScene("GamePlay");
+        Debug.Log("Click On Stage Mode");
+        SceneManager.LoadScene("LevelPage");
+    }
+    void OnClickCommunity()
+    {
+        Debug.Log("Click On Community");
+        SceneManager.LoadScene("Community");
+    }
+    void OnClickLeaderBoard()
+    {
+        Debug.Log("Click On Leader Board");
+        SceneManager.LoadScene("LeaderBoard");
     }
 }
