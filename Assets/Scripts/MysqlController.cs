57684172a60cdee8df6fdc4a848412df9bdc7b1b
Mr-again <840155767@qq.com>
2020-02-06 15:21:58 -0800

    mysql added

--- Assets/Scripts/MysqlController.cs
+++ Assets/Scripts/MysqlController.cs
@@ -1,19 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using Dapper;
+using System.Data;
+using MySql.Data.MySqlClient;
+using MySql.Data;
 
 public class MysqlController : MonoBehaviour
 {
-    
+    public string mysqlconnectionString = "Server=127.0.0.1;Database=test;Uid=root;Pwd=00000000;";
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        MySqlConnection my = OpenConnection();
+        Debug.Log(my);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    public MySqlConnection OpenConnection()
+    {
+        MySqlConnection connection = new MySqlConnection(mysqlconnectionString);
+        connection.Open();
+        return connection;
     }
 }
