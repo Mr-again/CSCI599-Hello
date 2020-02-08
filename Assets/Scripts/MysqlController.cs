6b860919f0e53b6b057baa5580d6df28922c71d0
Mr-again <840155767@qq.com>
2020-02-08 03:44:15 -0800

    mysql demo

--- Assets/Scripts/MysqlController.cs
+++ Assets/Scripts/MysqlController.cs
@@ -30,6 +30,23 @@ public class MysqlController : MonoBehaviour
     {
         MySqlConnection my = OpenConnection();
         Debug.Log(my);
+        my.Execute("insert into t_schools(Name,Address) values(@Name,@Address)", new { Name = "西南大学", Address = "重庆市北碚区天生路2号" });
+        //批量插入数据
+        List<School> schools = new List<School>()
+    {
+      new School() {Address="China·BeiJing",Title="清华大学" },
+      new School() {Address="杭州",Title="浙江大学" },
+      new School() {Address="不知道，US?",Title="哈弗大学" }
+    };
+        //在执行参数化的SQL时，SQL中的参数（如@title可以和数据表中的字段不一致，但要和实体类型的属性Title相对应）
+        my.Execute("insert into t_schools(Address,Name) values(@address,@title);", schools);
+        //通过匿名类型批量插入数据
+        my.Execute("insert into t_schools(Address,Name) values(@address,@name)",
+        new[] {
+      new {Address="杨浦区四平路1239号",Name="同济大学"},
+      new {Address="英国",Name="剑桥"},
+      new {Address="美国·硅谷",Name="斯坦福大学"}
+        });
     }
 
     // Update is called once per frame
@@ -43,4 +60,22 @@ public class MysqlController : MonoBehaviour
         connection.Open();
         return connection;
     }
+}
+
+class School
+{
+    /*
+      若属性名和数据库字段不一致（不区分大小写）则查询不出数据，如果使用EF则可以通过Column特性
+      建立属性和数据表字段之间的映射关系，Dapper则不行
+    */
+    //[Column("Name")]
+    public string Title { set; get; }
+    public string Address { set; get; }
+}
+
+class Student
+{
+    public string Name { set; get; }
+    public string Number { set; get; }
+    public int SchoolId { set; get; }
 }
