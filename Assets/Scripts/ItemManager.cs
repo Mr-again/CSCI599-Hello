f492d0761c10e22102c9fd1e717801e09fd81ccc
Jianing Wang <46771181+KristalW@users.noreply.github.com>
2020-02-29 21:13:20 -0800

    Add files via upload

---
+++ Assets/Scripts/ItemManager.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ItemManager : MonoBehaviour
+{
+    public int starNum;
+
+    public Button home;
+
+    public string scores;
+
+    public int score = 0;
+
+    public Text diamonds;
+
+    private string tempItem;
+    private void Awake()
+    {
+
+        home = GetComponent<Button>();
+        diamonds = GetComponent<Text>();
+    }
+
+
+    void CreateData()
+    {
+        for (int i = 0; i <=24; i++)
+        {
+            tempItem = "Grid/Item" + " (" + i.ToString() + ")";
+
+            starNum = 1;
+            score += starNum;
+            ShowStar(starNum, tempItem);
+        }
+        ShowScores(score);
+    }
+
+    // 
+    void ShowStar(int num, string level) 
+    {
+        if (num == 0)
+        {
+            transform.Find(level + "/star1").gameObject.SetActive(false);
+            transform.Find(level + "/star2").gameObject.SetActive(false);
+            transform.Find(level + "/star3").gameObject.SetActive(false);
+        }
+        if (num == 1)
+        {
+            transform.Find(level + "/star1").gameObject.SetActive(true);
+            transform.Find(level + "/star2").gameObject.SetActive(false);
+            transform.Find(level + "/star3").gameObject.SetActive(false);
+        }
+        if (num == 2)
+        {
+            transform.Find(level + "/star1").gameObject.SetActive(true);
+            transform.Find(level + "/star2").gameObject.SetActive(true);
+            transform.Find(level + "/star3").gameObject.SetActive(false);
+        }
+        if (num == 3)
+        {
+            transform.Find(level + "/star1").gameObject.SetActive(true);
+            transform.Find(level + "/star2").gameObject.SetActive(true);
+            transform.Find(level + "/star3").gameObject.SetActive(true);
+        }
+    }
+
+    void ShowScores(int number)
+    {
+        scores = score.ToString();
+        diamonds.text = scores;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        home.onClick.AddListener(() => { ClickHome(); });
+        CreateData();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ClickHome()
+    {
+        Debug.Log("Click on Home");
+        SceneManager.LoadScene("Enterance");
+    }
+}
+
---
+++ Assets/Scripts/LevelButton.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelButton : MonoBehaviour
+{
+    public Sprite lockSprite;
+
+    public Text levelText;
+
+    private int level = 0;
+
+    private Button button;
+
+    private Image image;
+
+    void OnEnable()
+    {
+        button = GetComponent<Button>();
+        image = GetComponent<Image>();
+
+    }
+
+    public void Setup(int level, bool isUnlock)
+    {
+        this.level = level;
+        levelText.text = level.ToString();
+
+        if (isUnlock)
+        {
+            image.sprite = null;
+            button.enabled = true;
+            levelText.gameObject.SetActive(true);
+        }
+        else
+        {
+            image.sprite = lockSprite;
+            button.enabled = false;
+            levelText.gameObject.SetActive(false);
+        }
+    }
+
+    public void onClick()
+    {
+
+    }
+
+}
