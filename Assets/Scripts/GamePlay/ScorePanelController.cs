63afb13e23e8c2d75c4a6a9871d61feb1e679529
Mr-again <840155767@qq.com>
2020-03-01 23:41:58 -0800

    retry button

---
+++ Assets/Scripts/GamePlay/ScorePanelController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ScorePanelController : MonoBehaviour
+{
+    public GameObject menu_panel;
+    public Button menu_button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        menu_button.onClick.AddListener(() => { onClickMenuButton(); });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void onClickMenuButton()
+    {
+        menu_panel.SetActive(true);
+    }
+}
