46d8f7ea3cd176fe4495d851867d61661198ca4a
Mr-again <840155767@qq.com>
2020-04-21 13:52:33 -0700

    screen shot

--- Assets/Scripts/DesignPage/MapMaker.cs
+++ Assets/Scripts/DesignPage/MapMaker.cs
@@ -14,6 +14,8 @@ using Json;
 
 public class MapMaker : MonoBehaviour
 {
+
+
     GameController gameController;
 
     public Button wall;
@@ -363,9 +365,30 @@ public class MapMaker : MonoBehaviour
 
     }
 
+    Texture2D CaptureScreenshot2(Rect rect)
+    {
+        // 先创建一个的空纹理，大小可根据实现需要来设置  
+        Texture2D screenShot = new Texture2D((int)rect.width, (int)rect.height, TextureFormat.RGB24, false);
+
+        // 读取屏幕像素信息并存储为纹理数据，  
+        screenShot.ReadPixels(rect, 0, 0);
+        screenShot.Apply();
+
+        // 然后将这些纹理数据，成一个png图片文件  
+        byte[] bytes = screenShot.EncodeToPNG();
+        string filename = Application.dataPath + "/Screenshot.png";
+        System.IO.File.WriteAllBytes(filename, bytes);
+        Debug.Log(string.Format("截屏了一张图片: {0}", filename));
+
+        // 最后，我返回这个Texture2d对象，这样我们直接，所这个截图图示在游戏中，当然这个根据自己的需求的。  
+        return screenShot;
+    }
+
     void OnClickSave()
     {
-        if(one_star.text == "" || two_star.text == "" || three_star.text == "")
+        CaptureScreenshot2(new Rect(Screen.width * 0.2f, Screen.height * 0f, Screen.width * 0.715f, Screen.height * 1f));
+
+        if (one_star.text == "" || two_star.text == "" || three_star.text == "")
         {
             Debug.Log("Must have value of star values");
             alert_text.text = "Must have value of star values";
